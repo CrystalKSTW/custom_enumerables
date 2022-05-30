103c9e1d28b209e3dc43dc0d88adb862e14a201f
CrystalKSTW <crystalkstw372000@gmail.com>
2022-05-30 22:37:14 +0630

    all finished

--- my_enumerables.rb
+++ my_enumerables.rb
@@ -113,4 +113,66 @@ module Enumerable
         end
         return bool
       end
+
+      def my_count(element = nil)
+        index = 0
+        total = 0
+        if block_given?
+          self.length.times do
+            if yield(self[index])
+              total += 1 
+              index += 1
+            else
+              index += 1
+            end
+          end
+        elsif element == nil
+          total = self.length
+        else
+          self.length.times do
+            if self[index] == element
+              total += 1
+              index += 1
+            else
+              index += 1
+            end
+          end
+        end
+        return total
+      end
+    
+      def my_map(var = nil)
+        index = 0
+        new_arr = []
+    
+        if block_given? && var == nil
+          self.length.times do 
+            new_arr.push(yield(self[index]))
+            index += 1
+          end
+        elsif block_given? && var.is_a?(Proc) || block_given? == false && var.is_a?(Proc)
+          self.length.times do 
+            new_arr.push(var.call(self[index]))
+            index += 1
+          end
+        else
+          return to_enum(:self)
+        end
+        return new_arr
+      end
+    
+      def my_inject(memo = nil)
+        index = 0
+        self.length.times do
+         
+          if memo == nil
+            memo = self[index]
+            index += 1
+            next
+          end
+          memo = yield(memo, self[index])
+          index += 1
+        end
+        return memo
+      end
 end
